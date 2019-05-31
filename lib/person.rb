a16569e9c58671fb3de88051b5738a9200d1de28
Lukie Kang <lukiek+f@gmail.com>
2019-05-31 16:35:02 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Dog
+
+  def intialize(name, job)
+    @name = name
+    @job = job
+  end
+
+  def name
+    @name
+  end
+  
+  def job
+    @job
+  end
+
+  def name=(new_name)
+    @name
+  def job=(new_job)
+    @job = new_job
+  end
+
+  
+end
