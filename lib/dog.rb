e50377a5dfd260898a6c3b51e1abcce63b670503
Lukie Kang <lukiek+f@gmail.com>
2019-05-31 16:35:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,7 +8,7 @@ class Dog
   def name
     @name
   end
-  
+
   def breed
     @breed
   end
@@ -16,12 +16,10 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
-  
+
   def breed=(new_breed)
     @breed = new_breed
   end
 
-  
+
 end
-
-
